013d472e531bc52444d5c6bdb162904062f05709
FellypeSouza <fellypedev23@gmail.com>
2024-10-01 17:05:01 -0300

    modifying the project

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,18 +1,44 @@
-import { Button, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import { Button, Text, StyleSheet, TextInput, View } from "react-native";
 import Resultado from "./Resultado";
 
 export default function Form(){
+
+    const [height, setHeight] = useState(null);
+    const [weight, setWeight] = useState(null);
+    const [messageImc, setMessageImc] = useState("Preencha o peso e a altura");
+    const [imc, setImc] = useState(null);
+    const [textButton, setTextButton] = useState("Calcular");
+
+    function calcularImc(){
+        return setImc((weight/(height * height).toFixed(2)));
+    }
+
+    function validarImc(){
+        if (weight != null && height != null) {
+            calcularImc();
+            setHeight(null);
+            setWeight(null);
+            setMessageImc("Seu IMC é igual a: ");
+            setTextButton("Calcular Novamente");
+            return
+        }
+        setImc(null);
+        setTextButton("Calcular");
+        setMessageImc("Preencha o peso e altura");
+    }
+
     return(
         <View>
             <View>
                 <Text>Altura</Text>
-                <TextInput placeholder="Altura" KeyboardType="numeric"/>
+                <TextInput style={styles.input} onChangeText={setHeight} value={height} placeholder="Altura" KeyboardType="numeric"/>
 
                 <Text>Peso</Text>
-                <TextInput placeholder="Peso" KeyboardType="numeric"/>
+                <TextInput onChangeText={setWeight} value={weight} placeholder="Peso" KeyboardType="numeric"/>
 
-                <Button title="Calcular"/>
-                <Resultado resultadoIMC = "2"/>
+                <Button onPress={()=>{validarImc()}} title="Calcular"/>
+                <Resultado resultadoText = {messageImc} resultadoIMC={imc}/>
             </View>
 
             <View></View>
@@ -20,3 +46,14 @@ export default function Form(){
         </View>
     );
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input: {
+        padding: 20,
+    }
+  });
